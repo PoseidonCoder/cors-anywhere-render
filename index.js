fb17d54a776e561a2abbff3d709a3b46b6c8ef45
PoseidonCoder <34260118+PoseidonCoder@users.noreply.github.com>
2022-12-08 20:31:16 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const cors_proxy = require("cors-anywhere");
+
+const host = "https://study-bot-3c3o.onrender.com";
+const port = 3000;
+
+cors_proxy
+  .createServer({
+    originWhitelist: [],
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, host, () => {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
+  
